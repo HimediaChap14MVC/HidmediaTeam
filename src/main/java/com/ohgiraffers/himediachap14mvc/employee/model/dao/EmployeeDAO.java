904815f2549a0ef0245ffe7e8dfd39ac7ef0866b
kimkeny <gustmd9146@gmail.com>
2024-08-27 12:32:37 +0900

    selectOneEmployeeID  finish in EmployeeService and selectOneId add in DAO

--- src/main/java/com/ohgiraffers/himediachap14mvc/employee/model/dao/EmployeeDAO.java
+++ src/main/java/com/ohgiraffers/himediachap14mvc/employee/model/dao/EmployeeDAO.java
@@ -1,4 +1,12 @@
 package com.ohgiraffers.himediachap14mvc.employee.model.dao;
 
+import com.ohgiraffers.himediachap14mvc.employee.model.dto.EmployeeDTO;
+
+import java.sql.Connection;
+
 public class EmployeeDAO {
+    public static EmployeeDTO selectOneId(Connection connection, String empId) {
+
+
+    }
 }
--- src/main/java/com/ohgiraffers/himediachap14mvc/employee/model/service/EmployeeService.java
+++ src/main/java/com/ohgiraffers/himediachap14mvc/employee/model/service/EmployeeService.java
@@ -1,17 +1,26 @@
 package com.ohgiraffers.himediachap14mvc.employee.model.service;
 
+import com.ohgiraffers.himediachap14mvc.employee.model.dao.EmployeeDAO;
 import com.ohgiraffers.himediachap14mvc.employee.model.dto.EmployeeDTO;
 
 import java.sql.Connection;
 
+import static com.ohgiraffers.himediachap14mvc.common.jdbc.JDBCTemplate.close;
+import static com.ohgiraffers.himediachap14mvc.common.jdbc.JDBCTemplate.getConnection;
+
 public class EmployeeService {
+
+    private EmployeeDAO employeeDAO = new EmployeeDAO();
 
     public EmployeeDTO selectOneEmployeeID(String empId) {
 
         /*커넥션 생성*/
-        
+        Connection connection = getConnection();
         /*정보전달 및 조회*/
+        EmployeeDTO selectedEmpId = employeeDAO.selectOneId(connection, empId);
         /*커넥션 닫기*/
+        close(connection);
         /*결과 반환*/
+        return selectedEmpId;
     }
 }
