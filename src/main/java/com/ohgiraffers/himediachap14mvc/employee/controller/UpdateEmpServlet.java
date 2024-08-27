4999140d16d8291389f470fb2cb972e77b1aeee3
yeJin4069 <jinny4069@gmail.com>
2024-08-27 12:09:07 +0900

    UpdateEmpServlet

--- src/main/java/com/ohgiraffers/himediachap14mvc/employee/controller/UpdateEmpServlet.java
+++ src/main/java/com/ohgiraffers/himediachap14mvc/employee/controller/UpdateEmpServlet.java
@@ -1,4 +1,34 @@
 package com.ohgiraffers.himediachap14mvc.employee.controller;
 
-public class UpdateEmpServlet {
+import com.ohgiraffers.himediachap14mvc.employee.model.service.EmployeeService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+
+@WebServlet("/employee/update")
+public class UpdateEmpServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String empId = req.getParameter("empId");
+        emp.setEmpId(empId);
+        emp.setEntDate(entDate);
+
+        int result = new EmployeeService().updateEmp(emp);
+
+        String path = "";
+        if(result > 0) {
+            path = "webapp/views/common/successPage.jsp";
+            req.setAttribute("success", "updateEmp");
+        } else {
+            path = "webapp/views/common/errorPage.jsp";
+            req.setAttribute("error", "퇴사여부수정실패");
+        }
+        req.getRequestDispatcher(path).forward(req, resp);
+    }
 }
