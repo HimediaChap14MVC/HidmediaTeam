7468284e94fb368a4000761f2f40a6f0c87d6e43
JingiHong <hjg0729@naver.com>
2024-08-27 12:57:07 +0900

    insert

--- src/main/java/com/ohgiraffers/himediachap14mvc/employee/controller/InsertEmpServlet.java
+++ src/main/java/com/ohgiraffers/himediachap14mvc/employee/controller/InsertEmpServlet.java
@@ -1,4 +1,34 @@
 package com.ohgiraffers.himediachap14mvc.employee.controller;
 
-public class InsertEmpServlet {
+import com.ohgiraffers.himediachap14mvc.employee.model.service.EmployeeService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.Date;
+
+@WebServlet("/employee/insert")
+public class InsertEmpServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String empName = req.getParameter("empName");
+        String empNo = req.getParameter("empNo");
+        String email = req.getParameter("email");
+        String phone = req.getParameter("phone");
+        String deptCode = req.getParameter("deptCode");
+        String jobCode = req.getParameter("jobCode");
+        String salLevel	= req.getParameter("salLevel");
+        int salary = Integer.parseInt(req.getParameter("salary"));
+        double bonus = Double.parseDouble(req.getParameter("bonus"));
+        String managerId = req.getParameter("managerId");
+        Date hireDate = Date.valueOf(req.getParameter("hireDate"));
+
+        EmployeeService empService = new EmployeeService();
+        String newEmpId = empService.selectNewEmpId();
+
+    }
 }
